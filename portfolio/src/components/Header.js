12a195201fbdba161dce0beadb8f286ea60fd33b
Natasha Beddow <beddow90@gmail.com>
2022-08-22 17:38:15 +0100

    added component files

---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "./Navigation";
+
+export default function Header() {
+  return (
+    <div>
+      <Nav />
+      <h1>Natasha Sunita Portfolio</h1>
+    </div>
+  );
+}
