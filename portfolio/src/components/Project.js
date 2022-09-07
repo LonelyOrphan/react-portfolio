c418a460c1898d71c1bd12181e37453c11516df9
Natasha Beddow <beddow90@gmail.com>
2022-09-07 17:33:42 +0100

    removed wrapping anchor tags

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -1,24 +1,21 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import { useNavigate } from "react-router-dom";
 
 export default function Project(props) {
   return (
-    <a href={props.projectURL}>
-      <Card style={{ width: "20rem" }}>
-        <Card.Img
-          varient="top"
-          src={`images/${props.image}`}
-          alt="Card image"
-        />
-        <Card.Body className="text-center bg-light">
-          <Card.Title>{props.title}</Card.Title>
-          <Card.Text>{props.descr}</Card.Text>
-          <Card.Link className="card-link" href={props.repoLink}>
-            Github Repo
-          </Card.Link>
-        </Card.Body>
-      </Card>
-    </a>
+    <Card style={{ width: "20rem" }}>
+      <Card.Img varient="top" src={`images/${props.image}`} alt="Card image" />
+      <Card.Body className="text-center bg-light">
+        <Card.Link href={props.projectURL}>{props.title}</Card.Link>
+        <Card.Text>{props.descr}</Card.Text>
+        <Card.Link className="card-link" href={props.repoLink}>
+          Github Repo
+        </Card.Link>
+      </Card.Body>
+    </Card>
   );
+}
+
+{
+  /* <a href={props.projectURL}></a> */
 }
